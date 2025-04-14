84dd9acc0df7eaa2fd82240a3d041b8ecd0397d7
Nikitososs <nsmirnow.n@gmail.com>
2025-04-14 13:38:46 +0600

    feat: add tests for quadratic solver

---
+++ quad_test.c
@@ -0,0 +1,93 @@
+#include "quad.h"
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_1() {
+  double eps = 0;
+  double a = 0;
+  double b = 1;
+  double c = -1;
+
+  double *res = quad(a, b, c, eps);
+  assert(res == NULL);
+}
+
+void test_2() {
+  double eps = 0;
+  double a = 1;
+  double b = 0;
+  double c = -1;
+
+  double *res = quad(a, b, c, eps);
+  assert(res[0] == 2 && res[1] == -1 && res[2] == 1);
+  free(res);
+}
+
+void test_3() {
+  double eps = 0;
+  double a = 1;
+  double b = 0;
+  double c = 0;
+
+  double *res = quad(a, b, c, eps);
+  assert(res[0] == 1 && res[1] == 0);
+  free(res);
+}
+
+void test_4() {
+  double eps = 0;
+  double a = 1;
+  double b = 0;
+  double c = 1;
+
+  double *res = quad(a, b, c, eps);
+  assert(res[0] == 0);
+  free(res);
+}
+
+void test_5() {
+  double eps = 1E-4;
+  double a = 1;
+  double b = 0;
+  double c = -1E-7;
+
+  double *res = quad(a, b, c, eps);
+  assert(res[0] == 2 && fabs(res[1] + 3E-4) < eps && fabs(res[2] - 3E-4) < eps);
+  free(res);
+}
+
+void test_6() {
+  double eps = 1E-11;
+  double a = 1;
+  double b = -1E+10;
+  double c = -1;
+
+  double *res = quad(a, b, c, eps);
+  assert(res[0] == 2 && fabs(res[1] + 1E-10) < eps &&
+         fabs(res[2] - 1E+10) < eps);
+  free(res);
+}
+
+void test_7() {
+  double eps = 1E-7;
+  double a = 1;
+  double b = 0;
+  double c = -1E-8;
+
+  double *res = quad(a, b, c, eps);
+  assert(res[0] == 1 && res[1] < eps);
+  free(res);
+}
+
+int main() {
+  test_1();
+  test_2();
+  test_3();
+  test_4();
+  test_5();
+  test_6();
+  test_7();
+  return 0;
+}
