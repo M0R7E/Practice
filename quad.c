7d5d2ca96177cfa5b0cb4d7ba5271a7a8a3836a8
Nikitososs <nsmirnow.n@gmail.com>
2025-04-17 18:11:21 +0600

    fix: add ret codes

--- quad.c
+++ quad.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double *quad(double a, double b, double c, double eps) {
-  if (fabs(a) <= eps)
-    return NULL;
+int quad(double a, double b, double c, double eps, double **result) {
+  if (fabs(a) <= eps) {
+    *result = NULL;
+    return NOT_A_QUAD;
+  }
 
   b = b / a;
   c = c / a;
@@ -14,13 +16,11 @@ double *quad(double a, double b, double c, double eps) {
   double desc = (b * b) - (4 * c);
 
   if (desc < -eps) {
-    double *result = malloc(sizeof(double));
-    result[0] = 0;
-    return result;
+    *result = NULL;
+    return NO_R_ROOTS;
   }
 
   if (desc > eps) {
-    double *result;
     double x1;
     double x2;
 
@@ -42,23 +42,21 @@ double *quad(double a, double b, double c, double eps) {
       x2 = svo;
     }
 
-    result = malloc(sizeof(double) * 3);
-    result[0] = 2;
-    result[1] = x1;
-    result[2] = x2;
+    *result = (double *)malloc(sizeof(double) * 2);
+    (*result)[0] = x1;
+    (*result)[1] = x2;
 
-    return result;
+    return TWO_ROOTS;
   } else if (fabs(desc) <= eps) {
-    double *result = malloc(sizeof(double) * 2);
+    *result = (double *)malloc(sizeof(double) * 1);
 
     double x1 = (-b) / 2;
 
     if (fabs(x1) == 0)
       x1 = fabs(x1);
 
-    result[0] = 1;
-    result[1] = x1;
+    (*result)[0] = x1;
 
-    return result;
+    return ONE_ROOT;
   }
 }
--- quad.h
+++ quad.h
@@ -1,1 +1,6 @@
-double *quad(double a, double b, double c, double eps);
+#define NOT_A_QUAD -1
+#define NO_R_ROOTS 0
+#define ONE_ROOT 1
+#define TWO_ROOTS 2
+
+int quad(double a, double b, double c, double eps, double **result);
