a0528548f064f70af94cc5d37a22a8bb423261b4
Nikitososs <nsmirnow.n@gmail.com>
2025-04-17 18:12:03 +0600

    test: edit tests for handling ret codes

--- quad_test.c
+++ quad_test.c
@@ -10,7 +10,9 @@ void test_1() {
   double b = 1;
   double c = -1;
 
-  double *res = quad(a, b, c, eps);
+  double *res;
+  int ret = quad(a, b, c, eps, &res);
+  assert(ret == NOT_A_QUAD);
   assert(res == NULL);
 }
 
@@ -20,8 +22,10 @@ void test_2() {
   double b = 0;
   double c = -1;
 
-  double *res = quad(a, b, c, eps);
-  assert(res[0] == 2 && res[1] == -1 && res[2] == 1);
+  double *res;
+  int ret = quad(a, b, c, eps, &res);
+  assert(ret == TWO_ROOTS);
+  assert(res[0] == -1 && res[1] == 1);
   free(res);
 }
 
@@ -31,8 +35,10 @@ void test_3() {
   double b = 0;
   double c = 0;
 
-  double *res = quad(a, b, c, eps);
-  assert(res[0] == 1 && res[1] == 0);
+  double *res;
+  int ret = quad(a, b, c, eps, &res);
+  assert(ret == ONE_ROOT);
+  assert(res[0] == 0);
   free(res);
 }
 
@@ -42,8 +48,9 @@ void test_4() {
   double b = 0;
   double c = 1;
 
-  double *res = quad(a, b, c, eps);
-  assert(res[0] == 0);
+  double *res;
+  int ret = quad(a, b, c, eps, &res);
+  assert(ret == NO_R_ROOTS);
   free(res);
 }
 
@@ -54,9 +61,11 @@ void test_5() {
   double c = -1E-7;
   double eps_for_res = 1E-4;
 
-  double *res = quad(a, b, c, eps);
-  assert(res[0] == 2 && fabs(res[1] + 3E-4) < eps_for_res &&
-         fabs(res[2] - 3E-4) < eps_for_res);
+  double *res;
+  int ret = quad(a, b, c, eps, &res);
+  assert(ret == TWO_ROOTS);
+  assert(fabs(res[0] + 3E-4) < eps_for_res &&
+         fabs(res[1] - 3E-4) < eps_for_res);
   free(res);
 }
 
@@ -66,9 +75,10 @@ void test_6() {
   double b = -1E+10;
   double c = -1;
 
-  double *res = quad(a, b, c, eps);
-  assert(res[0] == 2 && fabs(res[1] + 1E-10) < eps &&
-         fabs(res[2] - 1E+10) < eps);
+  double *res;
+  int ret = quad(a, b, c, eps, &res);
+  assert(ret == TWO_ROOTS);
+  assert(fabs(res[0] + 1E-10) < eps && fabs(res[1] - 1E+10) < eps);
   free(res);
 }
 
@@ -78,8 +88,10 @@ void test_7() {
   double b = 0;
   double c = -1E-8;
 
-  double *res = quad(a, b, c, eps);
-  assert(res[0] == 1 && res[1] < eps);
+  double *res;
+  int ret = quad(a, b, c, eps, &res);
+  assert(ret == ONE_ROOT);
+  assert(res[0] < eps);
   free(res);
 }
 
