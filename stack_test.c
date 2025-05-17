d9b2ad55d3f4da56e4a4fbc357c07be86fbaa140
prudovik <yaktoblin@gmail.com>
2025-05-17 21:10:32 +0600

    test: add tests

--- stack_test.c
+++ stack_test.c
@@ -2,22 +2,36 @@
 #include <assert.h>
 #include <stdio.h>
 
-void test_1() {
-  int *stack = Stack();
-  Push(stack, 777);
-  Push(stack, 123);
-  assert(isIn(stack) == 1);
+void push_test() {
+  int *st = stack();
+  push(st, 777);
+  push(st, 123);
+  assert(is_in(st) == 1);
 }
 
-void test_2() {
-  int *stack = Stack();
-  Push(stack, 777);
-  Push(stack, 123);
-  assert(Pop(stack) == 123 && Pop(stack) == 777 && isIn(stack) == 0);
+void pop_test() {
+  int *st = stack();
+  push(st, 777);
+  push(st, 123);
+  assert(pop(st) == 123 && pop(st) == 777 && is_in(st) == 0);
+}
+
+void empty_pop_test() {
+  int *st = stack();
+  assert(is_in(st) == 0 && pop(st) == 0);
+}
+
+void is_in_test() {
+  int *st = stack();
+  assert(is_in(st) == 0);
+  push(st, 3);
+  assert(is_in(st) == 1);
 }
 
 int main() {
-  test_1();
-  test_2();
+  push_test();
+  pop_test();
+  empty_pop_test();
+  is_in_test();
   return 0;
 }
