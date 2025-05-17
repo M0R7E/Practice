75f0fffb79241d636582db485ed0185d738a511e
prudovik <yaktoblin@gmail.com>
2025-05-17 21:09:45 +0600

    fix: remove exit_failure

--- stack.c
+++ stack.c
@@ -2,32 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *Stack() {
+int *stack() {
   int *stack = malloc(sizeof(int));
   stack[0] = 0;
   return stack;
 }
 
-int Push(int *stack, int value) {
+int push(int *stack, int value) {
   stack = realloc(stack, sizeof(stack) + sizeof(int));
   stack[0] = stack[0] + 1;
   stack[stack[0]] = value;
   return 0;
 }
 
-int isIn(int *stack) {
-  if (stack[0] == 0) {
-    return 0;
-  } else
-    return 1;
-}
+int is_in(int *stack) { return stack[0] != 0; }
 
-int Pop(int *stack) {
-  if (isIn(stack)) {
+int pop(int *stack) {
+  if (is_in(stack)) {
     int result = stack[stack[0]];
     stack = realloc(stack, sizeof(stack) - sizeof(int));
     stack[0] = stack[0] - 1;
     return result;
-  } else
-    exit(EXIT_FAILURE);
+  }
+  return 0;
 }
